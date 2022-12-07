57f87f36de9e15fe1e6ab66acf2e64fcb44ae918
Abderrahman-Magdi-Mohammad <120033747+Abder-r7manMagdi@users.noreply.github.com>
2022-12-08 01:41:01 +0200

    Add files via upload

---
+++ abstract/src/MyOnlineShop/main.java
@@ -0,0 +1,14 @@
+package MyOnlineShop;
+
+public class main {
+    public static void main ( String[] args ) {
+        Product [] p = new Product[3];
+        p[0]=new Book ();
+        Product b1=new Book ();
+         b1.setPrice ( 100 );
+        b1.display ();
+         Product b2=new ChildrenBook ();
+        b2.setPrice ( 100 );
+        b2.display ();
+    }
+}
